89afc7858a24bca73ca4bc88d0d83078b653a009
JustinasZelvys <zelvys.justinas@gmail.com>
2024-06-24 18:48:57 +0300

    Contact and Create Post Hero Image At The Top And design Improvments

--- src/Components/CreatePost.js
+++ src/Components/CreatePost.js
@@ -17,34 +17,41 @@ const CreatePost = ({ onSave }) => {
   };
 
   return (
-    <div className="create-post-container">
-      <h2>Create a New Post</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <textarea
-          placeholder="Content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Image URL"
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Author"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <button type="submit">Save</button>
-      </form>
+    <div>
+      <div className="create-post-hero">
+        <div className="overlay">
+          <h1>Create Post</h1>
+        </div>
+      </div>
+      <div className="create-post-container">
+        <h2>Create a New Post</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Image URL"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <button type="submit">Save</button>
+        </form>
+      </div>
     </div>
   );
 };
